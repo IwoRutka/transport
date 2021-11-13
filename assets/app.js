966423fa23dd39d9f3c41851f12ed7f3d0c69f12
iwor <web@fotax.pl>
2021-11-13 23:06:46 +0100

    menu & header

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,20 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+import './styles/all.css';
 import './styles/app.css';
+import './styles/helper/sidebar.css';
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './js/scripts.js';
+import './js/lib/sidebar.js';
 
 // start the Stimulus application
 import './bootstrap';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
+
+
+// create global $ and jQuery variables
+ global.$ = global.jQuery = $;
